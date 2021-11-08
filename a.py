a6bac44ef81e812aa6ee8706fe1429a3e30e19bf
Ray <80833913+trongkhoile@users.noreply.github.com>
2021-11-08 22:57:21 +0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,45 @@
+
+import pyttsx3
+import speech_recognition
+from datetime import date
+while True:
+        
+                robot_ear = speech_recognition.Recognizer()
+                with speech_recognition.Microphone() as mic:
+                        print("Robot:I'm Listening")
+                        audio= robot_ear.listen(mic)
+                print("...")
+                try:
+                  you = robot_ear.recognize_google(audio)
+                except:
+                  you=""
+
+                print("You:",you)   
+
+                if "hello" in you :
+                        robot_brain="hello my friend"
+                elif "how are you" in you:
+                        robot_brain="i'm fine and you"
+                elif "my name" in you:
+                        robot_brain="Khoi"
+                elif "your name" in you:
+                        robot_brain="my name is Ray"   
+                elif "today" in you:
+                        today = date.today()
+                        robot_brain = today.strftime("%B %d, %Y")
+                elif "wow" in you:
+                        robot_brain="thank you" 
+                elif "bye" in you:
+                        robot_brain="bye my friend"
+                        print(robot_brain)
+                        engine = pyttsx3.init()
+                        engine.say(robot_brain)
+                        engine.runAndWait()
+                        break
+                else :
+                        robot_brain="i can't hear you"    
+                print(robot_brain)
+                engine = pyttsx3.init()
+                engine.say(robot_brain)
+                engine.runAndWait()
+               
